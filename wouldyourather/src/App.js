ef77dc02368b8e05757e4cf87a7b02604fa547a8
yufrances90 <yufrances90@gmail.com>
2018-11-20 18:07:35 -0800

    feat: Included customized Navbar

--- wouldyourather/src/App.js
+++ wouldyourather/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 import PLogin from './pages/PLogin';
+import CNavbar from './components/CNavbar';
 
 class App extends Component {
 
     render() {
         return (
             <div className="App">
+                <CNavbar />
                 <PLogin />
             </div>
         );
